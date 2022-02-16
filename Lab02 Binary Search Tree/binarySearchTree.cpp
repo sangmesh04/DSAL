015531c82dda3c660a62bf289a4cfb483047e589
sangmesh04 <78370367+sangmesh04@users.noreply.github.com>
2022-02-16 19:12:09 +0530

    done with assig 02

--- Lab02 Binary Search Tree/binarySearchTree.cpp
+++ Lab02 Binary Search Tree/binarySearchTree.cpp
@@ -9,75 +9,252 @@ A Dictionary stores keywords and its meanings. Provide facility for
 Use Binary Search Tree for implementation.
 
 */
-#include<iostream>
-#include<string>
+
+#include"iostream"
+#include<string.h>
 using namespace std;
 
 struct node{
-int key;
-int mean;
-struct node * right;
-struct node * left;
+ char k[20];
+ char m[20];
+ struct node  *left;
+ struct node * right;
 };
 
-class BinarySearchTree{
+class dictionary
+{
 public:
-    node *root;
-    BinarySearchTree();
-    node * createBSTree(node *);
-    node * CreateNode();
-    void inorder(node *);
+ node *root;
+ void create();
+ void display(node *, int order);
+ void insertNode(node * root,node *temp);
+ int searchNode(node *,char []);
+ int updateNode(node *,char []);
+ node * deleteNode(node *,char []);
+ node * leftMost(node *);
 };
 
-BinarySearchTree::BinarySearchTree(){
-    root = NULL;
+void dictionary ::create(){
+ node *temp;
+ int ch;
+ do
+ {
+  temp = new node;
+  cout<<"\nKey: ";
+  cin>>temp->k;
+  cout<<"Mean: ";
+  cin>>temp->m;
+
+  temp->left = NULL;
+  temp->right = NULL;
+
+  if(root == NULL){
+   root = temp;
+  }else{
+   insertNode(root, temp);
+  }
+  cout<<"Do you want to add more (1/0): ";
+  cin>>ch;
+ }
+ while(ch == 1);
+
 }
 
-node * BinarySearchTree::CreateNode(){
-    node *p = new node;
-    p->left = NULL;
-    p->right = NULL;
-    return p;
-}
-
-node * BinarySearchTree::createBSTree(node *p){
-    int k,m;
-    cout<<p<<endl;
-    cout<<"#Key -->(x to stop): ";
-    cin>>k;
-    if(k==0){
-        return NULL;
-    }else{
-         cout<<"@Meaning --> ";
-         cin>>m;
-         node *q = CreateNode();
-         q->key = k;
-         q->mean = m;
-         if(p == NULL){
-            node *q = CreateNode();
-            createBSTree(q);
-        }else if(p->key > k){
-                p->left = createBSTree(p->left);
-        }else{
-                p->right = createBSTree(p->right);
-            }
-        return q;
+void dictionary::insertNode(node * root,node *temp){
+ if(strcmp(temp->k, root->k) < 0 ){
+  if(root->left == NULL){
+    root->left = temp;
+  }
+  else{
+    insertNode(root->left,temp);
+ }
+ }
+ else if (strcmp(temp->k, root->k) > 0 ){
+    if(root->right == NULL){
+      root->right = temp;
     }
+  else{
+   insertNode(root->right,temp);
+ }
+ }else{
+ cout<<"Key must be distinct!"<<endl;
+ }
 }
 
-void BinarySearchTree::inorder(node *temp){
-        if(temp != NULL){
-             inorder(temp->left);
-             cout<<temp->key<<" : "<<temp->mean<<", ";
-             inorder(temp->right);
+void dictionary::display(node * root, int order){
+ if( root != NULL && order == 1){
+  display(root->left,order);
+  cout<<root->k<<" : "<<root->m<<endl;
+  display(root->right,order);
+ }
+ if( root != NULL && order == 2){
+  display(root->right,order);
+  cout<<root->k<<" : "<<root->m<<endl;
+  display(root->left,order);
+ }
+}
+
+int dictionary::searchNode(node * root,char k[20]){
+ int c=0;
+ while(root != NULL){
+  c++;
+  if(strcmp(k,root->k) == 0){
+   cout<<"\nNum of Comparisons: "<<c<<endl;
+   return 1;
+  }
+  if(strcmp (k, root->k) < 0){
+   root = root->left;
+  }
+  if(strcmp (k, root->k) > 0){
+   root = root->right;
+ }
+ }
+ return -1;
+}
+
+int dictionary::updateNode(node * root,char k[20])
+{
+ while(root != NULL){
+  if(strcmp(k,root->k) == 0){
+   cout<<"Enter new meaning of key "<<root->k<<" : ";
+   cin>>root->m;
+   return 1;
+  }
+  if(strcmp (k, root->k) < 0){
+   root = root->left;
+  }
+  if(strcmp (k, root->k) > 0){
+   root = root->right;
+ }
+ }
+ return -1;
+}
+
+node* dictionary::deleteNode(node * root,char k[20]){
+ node *temp;
+ if(root == NULL){
+  cout<<"Key is not present in dic!"<<endl;
+  return root;
+ }
+
+ if (strcmp(k,root->k) < 0){
+  root->left = deleteNode(root->left, k);
+  return root;
+ }
+ if (strcmp(k,root->k) > 0){
+   root->right = deleteNode(root->right, k);
+   return root;
+ }
+
+ if (root->right==NULL&&root->left==NULL){
+  temp = root;
+  delete temp;
+  cout<<"Deleted!"<<endl;
+  return NULL;
+}
+
+  if(root->right==NULL){
+  temp = root;
+  root = root->left;
+  delete temp;
+  cout<<"Deleted!"<<endl;
+  return root;
+  }
+  else if(root->left==NULL){
+  temp = root;
+  root = root->right;
+  delete temp;
+  cout<<"Deleted!"<<endl;
+  return root;
+  }
+  temp = leftMost(root->right);
+  strcpy(root->k,temp->k);
+  root->right = deleteNode(root->right, temp->k);
+  return root;
+}
+
+node * dictionary:: leftMost(node *q){
+ while(q->left != NULL){
+  q = q->left;
+ }
+ return q;
+}
+
+
+
+int main()
+{
+ int ch = 1,order;
+ dictionary d;
+ d.root = NULL;
+
+ while(ch>0 && ch<6){
+  cout<<"\n<---- Menu ---->"<<endl;
+  cout<<"1. Add a key\n2. Display dic\n3. Search a key\n4. Update a key\n5. Delete a key"<<endl;
+  cout<<"Enter choice(0 to exit): ";
+  cin>>ch;
+
+  switch(ch){
+case 1: d.create();
+  break;
+case 2: if(d.root == NULL){
+  cout<<"Dic is empty!"<<endl;
+  }
+  else{
+        cout<<"1. Ascending\n2. Descending"<<endl;
+        cout<<"Enter option: ";
+        cin>>order;
+        cout<<endl;
+        if(order==1 || order == 2){
+            d.display(d.root,order);
+        }else{
+            cout<<"Didn't get that..."<<endl;
         }
-}
-
-int main(){
-    BinarySearchTree obj;
-
-    obj.createBSTree(NULL);
-    node *p = obj.CreateNode();
-    obj.inorder(p);
-    //cout<<"\n"<<p;
+  }
+  break;
+case 3: if(d.root == NULL){
+  cout<<"Dic is empty!"<<endl;
+ }else{
+  cout<<"Enter key to be searched: ";
+  char k[20];
+  cin>>k;
+  if( d.searchNode(d.root,k) == 1){
+  cout<<"Key is present!"<<endl;
+  }
+  else{
+  cout<<"Key is absent!"<<endl;
+  }
+ }
+  break;
+case 4:
+  if(d.root == NULL){
+  cout<<"Dic is empty!"<<endl;
+ }else{
+  cout<<"Enter key to be updated: ";
+  char k[20];
+  cin>>k;
+  if(d.updateNode(d.root,k) == 1){
+  cout<<"Updated!"<<endl;
+  }else{
+  cout<<"Key is absent!"<<endl;
+  }
+ }
+  break;
+case 5:
+  if(d.root == NULL){
+  cout<<"Dic is empty!"<<endl;
+  }else{
+  cout<<"Enter key to be deleted: ";
+  char k[20];
+  cin>>k;
+  if(d.root == NULL){
+  cout<<"Key is absent!";
+  }
+  else{
+  d.root = d.deleteNode(d.root,k);
+    }
+   }
+  }
+ }
+ return 0;
 }
